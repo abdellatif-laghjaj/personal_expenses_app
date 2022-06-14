fa108e48dde08002a7f33fe29da9b453012d9b83
Abdellatif Laghjaj <abdelatiflaghjaj@gmail.com>
2022-06-15 00:11:18 +0100

    fix bottom sheet

--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -52,66 +52,73 @@ class _NewTransactionState extends State<NewTransaction> {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      child: Container(
-        padding: EdgeInsets.all(12),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.end,
-          children: [
-            TextField(
-              controller: _titleControler,
-              onSubmitted: (_) => _submitData(),
-              decoration: InputDecoration(
-                labelText: "Title",
-              ),
-            ),
-            TextField(
-              keyboardType: TextInputType.number,
-              controller: _amountControler,
-              onSubmitted: (_) => _submitData(),
-              decoration: InputDecoration(
-                labelText: "Amount",
-              ),
-            ),
-            Container(
-              margin: EdgeInsets.only(top: 14),
-              child: Row(
-                children: [
-                  Expanded(
-                    child: Text(
-                      _selectedDate == null
-                          ? "No Date Chosen"
-                          : "Picked Date: ${DateFormat.yMd().format(_selectedDate)}",
-                    ),
-                  ),
-                  FlatButton(
-                    textColor: Theme.of(context).primaryColor,
-                    child: Text(
-                      "Select Date",
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    onPressed: _presentDatePicker,
-                  ),
-                ],
-              ),
-            ),
-            Container(
-              margin: EdgeInsets.only(top: 10),
-              // ignore: deprecated_member_use
-              child: RaisedButton(
-                onPressed: () => _submitData(),
-                child: Text("Add Transaction"),
-                color: Theme.of(context).primaryColor,
-                textColor: Theme.of(context).textTheme.button.color,
-                padding: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(99),
+    return SingleChildScrollView(
+      child: Card(
+        child: Container(
+          padding: EdgeInsets.only(
+            top: 10,
+            left: 10,
+            right: 10,
+            bottom: MediaQuery.of(context).viewInsets.bottom + 10,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              TextField(
+                controller: _titleControler,
+                onSubmitted: (_) => _submitData(),
+                decoration: InputDecoration(
+                  labelText: "Title",
                 ),
               ),
-            )
-          ],
+              TextField(
+                keyboardType: TextInputType.number,
+                controller: _amountControler,
+                onSubmitted: (_) => _submitData(),
+                decoration: InputDecoration(
+                  labelText: "Amount",
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(top: 14),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: Text(
+                        _selectedDate == null
+                            ? "No Date Chosen"
+                            : "Picked Date: ${DateFormat.yMd().format(_selectedDate)}",
+                      ),
+                    ),
+                    FlatButton(
+                      textColor: Theme.of(context).primaryColor,
+                      child: Text(
+                        "Select Date",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      onPressed: _presentDatePicker,
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(top: 10),
+                // ignore: deprecated_member_use
+                child: RaisedButton(
+                  onPressed: () => _submitData(),
+                  child: Text("Add Transaction"),
+                  color: Theme.of(context).primaryColor,
+                  textColor: Theme.of(context).textTheme.button.color,
+                  padding: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(99),
+                  ),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
