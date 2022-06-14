3684efa22e0a296ad765a9adf3ff7d4119a9ab5a
Abdellatif Laghjaj <abdelatiflaghjaj@gmail.com>
2022-06-14 01:05:08 +0100

    improve the charts

--- lib/widgets/chart.dart
+++ lib/widgets/chart.dart
@@ -26,7 +26,7 @@ class Chart extends StatelessWidget {
         "day": DateFormat.E().format(weekDay).substring(0, 1),
         "amount": totalSum
       };
-    });
+    }).reversed.toList();
   }
 
   double get totalSpending {
