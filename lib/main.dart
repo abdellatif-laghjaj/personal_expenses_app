f4efb8d1b89b1ac400ff1c0577ae761cb6f06fdf
Abdellatif Laghjaj <abdelatiflaghjaj@gmail.com>
2022-06-14 23:19:13 +0100

    responsive design

--- lib/main.dart
+++ lib/main.dart
@@ -172,14 +172,14 @@ class _MyHomePageState extends State<MyHomePage> {
               height: (MediaQuery.of(context).size.height -
                       appBar.preferredSize.height -
                       MediaQuery.of(context).padding.top) *
-                  0.4,
+                  0.3,
               child: Chart(_recentTransactions),
             ),
             Container(
               height: (MediaQuery.of(context).size.height -
                       appBar.preferredSize.height -
                       MediaQuery.of(context).padding.top) *
-                  0.6,
+                  0.7,
               child: TransactionList(_userTransactions, _deleteTransaction),
             ),
           ],
--- lib/widgets/chart_bar.dart
+++ lib/widgets/chart_bar.dart
@@ -9,40 +9,45 @@ class ChartBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Container(
-          height: 20,
-          child: FittedBox(
-            child: Text("\$${spendingAmount.toStringAsFixed(0)}"),
-          ),
-        ),
-        SizedBox(height: 4),
-        Container(
-          height: 60,
-          width: 10,
-          child: Stack(children: [
-            Container(
-              decoration: BoxDecoration(
-                color: Color.fromRGBO(220, 220, 220, 1),
-                border: Border.all(color: Colors.grey, width: 1),
-                borderRadius: BorderRadius.circular(10),
-              ),
+    return LayoutBuilder(builder: (context, constraints) {
+      return Column(
+        children: [
+          Container(
+            height: constraints.maxHeight * 0.15,
+            child: FittedBox(
+              child: Text("\$${spendingAmount.toStringAsFixed(0)}"),
             ),
-            FractionallySizedBox(
-              heightFactor: spendingPctOfTotal,
-              child: Container(
+          ),
+          SizedBox(height: constraints.maxHeight * 0.05),
+          Container(
+            height: constraints.maxHeight * 0.6,
+            width: 10,
+            child: Stack(children: [
+              Container(
                 decoration: BoxDecoration(
-                  color: Theme.of(context).accentColor,
+                  color: Color.fromRGBO(220, 220, 220, 1),
+                  border: Border.all(color: Colors.grey, width: 1),
                   borderRadius: BorderRadius.circular(10),
                 ),
               ),
-            ),
-          ]),
-        ),
-        SizedBox(height: 4),
-        Text(label),
-      ],
-    );
+              FractionallySizedBox(
+                heightFactor: spendingPctOfTotal,
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Theme.of(context).accentColor,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+              ),
+            ]),
+          ),
+          SizedBox(height: constraints.maxHeight * 0.05),
+          Container(
+            height: constraints.maxHeight * 0.15,
+            child: Text(label),
+          ),
+        ],
+      );
+    });
   }
 }
